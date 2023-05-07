6d81de97d7e25caf8d0289d0dfc4a0cc744f621b
Ethan Johnston <davidethanj11@gmail.com>
2023-05-07 16:46:02 -0700

    removed some useless statements

--- utils.py
+++ utils.py
@@ -79,7 +79,6 @@ def summary_from_summary_docs(summary_docs, initial_chain, final_sum_list, api_k
     else:
         max_tokens = 3800 - int(count)
         model = 'gpt-3.5-turbo'
-    print(summaries)
     final_llm = ChatOpenAI(openai_api_key=api_key, temperature=0, max_tokens=max_tokens, model_name=model)
     final_sum_list[2] = final_llm
     final_sum_chain = summarize_chain_creator(final_sum_list)
