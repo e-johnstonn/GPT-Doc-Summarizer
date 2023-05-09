ffc562307c1a211c761ab36ec0afe1f041696911
Ethan Johnston <davidethanj11@gmail.com>
2023-05-08 19:28:56 -0700

    Reorganized methods, added documentation, removed some redundant code

--- streamlit_app_utils.py
+++ streamlit_app_utils.py
@@ -1,9 +1,8 @@
 import tempfile
 
 import PyPDF2
-import pdfplumber
+
 from io import StringIO
-import re
 
 from langchain.chat_models import ChatOpenAI
 
@@ -11,6 +10,13 @@ from utils import doc_to_text, token_counter
 
 
 def pdf_to_text(pdf_file):
+    """
+    Convert a PDF file to a string of text.
+
+    :param pdf_file: The PDF file to convert.
+
+    :return: A string of text.
+    """
     pdf_reader = PyPDF2.PdfReader(pdf_file)
     text = StringIO()
     for i in range(len(pdf_reader.pages)):
@@ -19,56 +25,31 @@ def pdf_to_text(pdf_file):
     return text.getvalue().encode('utf-8')
 
 
-# attempt at auto - extracting table of contents
-# def find_toc_page(file_path):
-#     with pdfplumber.open(file_path) as pdf:
-#         for i, page in enumerate(pdf.pages):
-#             text = page.extract_text()
-#             if re.search(r'\bTable of Contents\b', text, re.IGNORECASE):
-#                 return i
-#         return None
-#
-#
-# def extract_toc(file_path, toc_page_num):
-#     toc = []
-#     with pdfplumber.open(file_path) as pdf:
-#         text = pdf.pages[toc_page_num].extract_text()
-#         for line in text.split('\n'):
-#             match = re.match(r'([\w\s\d,.:;-]+)', line)
-#             if match:
-#                 title = match.group().strip()
-#                 toc.append(title)
-#     return toc
-#
-#
-# def extract_sections(file_path, toc):
-#     sections = {}
-#     current_section = ""
-#     with pdfplumber.open(file_path) as pdf:
-#         for page in pdf.pages:
-#             text = page.extract_text()
-#             for line in text.split('\n'):
-#                 # Check if the line is one of the section titles
-#                 if line.strip() in toc:
-#                     current_section = line.strip()
-#                     sections[current_section] = ""
-#                 # Append the text to the current section
-#                 elif current_section:
-#                     sections[current_section] += f"{line}\n"
-#     return sections
-
-
 def check_gpt_4(api_key):
+    """
+    Check if the user has access to GPT-4.
+
+    :param api_key: The user's OpenAI API key.
+
+    :return: True if the user has access to GPT-4, False otherwise.
+    """
     try:
         ChatOpenAI(openai_api_key=api_key, model_name='gpt-4').call_as_llm('Hi')
-        print('User has GPT-4 access')
         return True
     except Exception as e:
-        print('No GPT-4 access')
         return False
 
 
-def token_limit(doc, maximum: 200000): #checks how many tokens are in a doc, returns false if it exceeds the limit set
+def token_limit(doc, maximum: 200000):
+    """
+    Check if a document has more tokens than a specified maximum.
+
+    :param doc: The langchain Document object to check.
+
+    :param maximum: The maximum number of tokens allowed.
+
+    :return: True if the document has less than the maximum number of tokens, False otherwise.
+    """
     text = doc_to_text(doc)
     count = token_counter(text)
     if count > maximum:
@@ -77,6 +58,15 @@ def token_limit(doc, maximum: 200000): #checks how many tokens are in a doc, ret
 
 
 def token_minimum(doc, minimum:2000):
+    """
+    Check if a document has more tokens than a specified minimum.
+
+    :param doc: The langchain Document object to check.
+
+    :param minimum: The minimum number of tokens allowed.
+
+    :return: True if the document has more than the minimum number of tokens, False otherwise.
+    """
     text = doc_to_text(doc)
     count = token_counter(text)
     if count < minimum:
@@ -85,6 +75,13 @@ def token_minimum(doc, minimum:2000):
 
 
 def check_key_validity(api_key):
+    """
+    Check if an OpenAI API key is valid.
+
+    :param api_key: The OpenAI API key to check.
+
+    :return: True if the API key is valid, False otherwise.
+    """
     try:
         ChatOpenAI(openai_api_key=api_key).call_as_llm('Hi')
         print('API Key is valid')
@@ -96,6 +93,13 @@ def check_key_validity(api_key):
 
 
 def create_temp_file(uploaded_file):
+    """
+    Create a temporary file from an uploaded file.
+
+    :param uploaded_file: The uploaded file to create a temporary file from.
+
+    :return: The path to the temporary file.
+    """
     with tempfile.NamedTemporaryFile(delete=False, suffix='.txt') as temp_file:
         if uploaded_file.type == 'application/pdf':
             temp_file.write(pdf_to_text(uploaded_file))
@@ -105,6 +109,15 @@ def create_temp_file(uploaded_file):
 
 
 def create_chat_model(api_key, use_gpt_4):
+    """
+    Create a chat model ensuring that the token limit of the overall summary is not exceeded - GPT-4 has a higher token limit.
+
+    :param api_key: The OpenAI API key to use for the chat model.
+
+    :param use_gpt_4: Whether to use GPT-4 or not.
+
+    :return: A chat model.
+    """
     if use_gpt_4:
         return ChatOpenAI(openai_api_key=api_key, temperature=0, max_tokens=500, model_name='gpt-3.5-turbo')
     else:
