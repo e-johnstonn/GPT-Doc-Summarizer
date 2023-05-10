f5f91f07e7c5177ec24cd05d4fb0f64bbd6dd9ce
Ethan Johnston <davidethanj11@gmail.com>
2023-05-09 22:53:05 -0700

    little text changes

--- main.py
+++ main.py
@@ -27,12 +27,14 @@ def main():
     if input_method == 'Enter a YouTube URL':
         youtube_url = st.text_input("Enter a YouTube URL to summarize")
 
-    api_key = st.text_input("Free usage cap has been hit! Enter your own API key here, or contact the author if you don't have one.")
+    api_key = st.text_input("Enter API key here, or contact the author if you don't have one.")
     st.markdown('[Author email](mailto:ethanujohnston@gmail.com)')
     use_gpt_4 = st.checkbox("Use GPT-4 for the final prompt (STRONGLY recommended, requires GPT-4 API access)", value=True)
     find_clusters = st.checkbox('Find optimal clusters (experimental, could save on token usage)', value=False)
     st.sidebar.markdown('# Made by: [Ethan](https://github.com/e-johnstonn)')
     st.sidebar.markdown('# Git link: [Docsummarizer](https://github.com/e-johnstonn/docsummarizer)')
+    st.sidebar.markdown("""<small>It's always good practice to verify that a website is safe before giving it your API key. 
+                        This site is open source, so you can check the code yourself, or run the streamlit app locally.</small>""", unsafe_allow_html=True)
 
 
     if st.button('Summarize (click once and wait)'):
